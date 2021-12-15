df2b0c1dc1e8a929bae239e5975b846c2444ee58
Kyra Christensen <christensen.kyral@gmail.com>
2021-12-14 16:20:40 -0800

    did a tdd on renderCandy

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,24 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
-
+import { renderCandy } from '../render-utils.js';
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('renderCandy should return a DOM element', (expect) => {
     //Arrange
+    let candy = 
+    {
+        name: 'skittles',
+        color: 'rainbow',
+        price: '3.99',
+    };
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = '<div class="candy-detail"><p class="name">skittles</p><p class="color">rainbow</p><p class="price">3.99</p></div>';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = renderCandy(candy);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual.outerHTML, expected, 'renderCandy returns a div');
 });
